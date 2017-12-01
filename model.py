632bf56c56bc22022b6fa80a44b48c1d609dc776
arubior <antrubio10@gmail.com>
2017-12-01 10:23:18 +0100

    trying thins with bi-lstm

--- model.py
+++ model.py
@@ -7,19 +7,20 @@ import torch.autograd as autograd
 class Network(nn.Module):
     """Bi-LSTM network."""
 
-    def __init__(self, input_dim, hidden_dim):
+    def __init__(self, input_dim, hidden_dim, batch_first=False):
         """Create the network."""
         super(Network, self).__init__()
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
-        self.lstm = nn.LSTM(input_dim, hidden_dim, bidirectional=True)
+        self.lstm = nn.LSTM(input_dim, hidden_dim,
+                            batch_first=batch_first, bidirectional=True)
 
     def forward(self, data, hidden):
         """Do a forward pass."""
         return self.lstm(data, hidden)
 
-    def init_hidden(self):
+    def init_hidden(self, batch_size):
         """Initialize the hidden state and cell state."""
-        return (autograd.Variable(torch.randn(2, 1, self.hidden_dim // 2)),
-                autograd.Variable(torch.randn(2, 1, self.hidden_dim // 2)))
+        return (autograd.Variable(torch.randn(2, batch_size, self.hidden_dim)),
+                autograd.Variable(torch.randn(2, batch_size, self.hidden_dim)))
 
--- use_model.py
+++ use_model.py
@@ -6,7 +6,7 @@ from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 from model import Network
 
 
-def create_packed_seq(data_dim, seq_lens):
+def create_packed_seq(data_dim, seq_lens, batch_first=False):
     """Create a packed input of sequences for a RNN."""
     seqs = [autograd.Variable(torch.randn(data_dim, sl)) for sl in seq_lens]
     t_seqs = []
@@ -17,22 +17,25 @@ def create_packed_seq(data_dim, seq_lens):
             t_seqs.append(seq)
 
     t_seqs = torch.stack(t_seqs)  # t_seqs is (batch size, data_dim, max length)
-    t_seqs = t_seqs.permute(0, 2, 1)  # now it is (batch size, max length, data_dim)
+    t_seqs = t_seqs.permute(2, 0, 1)  # now it is (batch size, max length, data_dim)
 
-    return pack_padded_sequence(t_seqs, seq_lens, batch_first=True)
+    return pack_padded_sequence(t_seqs, seq_lens, batch_first=batch_first)
 
 
 def main():
     """Forward sequences."""
     seq_lens = [5, 5, 3, 1]  # batch size = 4, max length = 5
+    batch_size = len(seq_lens)
     input_dim = 100
-    hidden_dim = 150
-    data = create_random_packed_seq(input_dim, seq_lens)
+    hidden_dim = 300
+    data = create_packed_seq(input_dim, seq_lens)
+    batch_first = True
 
-    model = Network(input_dim, hidden_dim)
-    hidden = model.init_hidden()
+    model = Network(input_dim, hidden_dim, batch_first)
+    hidden = model.init_hidden(batch_size)
     out, hidden = model.forward(data, hidden)
-    out, _ = pad_packed_sequence(out, batch_first=True)  # 2nd output are the sequence lengths
+    out, _ = pad_packed_sequence(out, batch_first=batch_first)  # 2nd output are the sequence lengths
+    print out
 
 
 if __name__ == '__main__':
