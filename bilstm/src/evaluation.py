Added normalization in evaluation, fixed f/bw_hiddens dimensions

bilstm/src/evaluation.py
@@ -61,11 +61,25 @@ class Evaluation(object):
             print("Key %s not in the precomputed features." % d)
             import epdb
             epdb.set_trace()
+        # init_hidden = (torch.autograd.Variable(torch.zeros(2, im_feats.size(0), 512)),
+                       # torch.autograd.Variable(torch.zeros(2, im_feats.size(0), 512)))
+        init_hidden = (torch.autograd.Variable(torch.zeros(2, 1, 512)),
+                       torch.autograd.Variable(torch.zeros(2, 1, 512)))
+
         if self.cuda:
             im_feats = im_feats.cuda()
-        out, _ = self.model.lstm(torch.autograd.Variable(im_feats).unsqueeze(0))
+            init_hidden = (init_hidden[0].cuda(), init_hidden[1].cuda())
+
+        im_feats = torch.nn.functional.normalize(im_feats, p=2, dim=1)
+        out, _ = self.model.lstm(torch.autograd.Variable(im_feats).unsqueeze(0),
+                                 init_hidden)
         out = out.data
+
+        fw_hiddens = out[0, :im_feats.size(0), :out.size(2) // 2]
+        bw_hiddens = out[0, :im_feats.size(0), out.size(2) // 2:]
+
         im_feats = torch.from_numpy(np.array(list(test_feats.values())))
+        im_feats = torch.nn.functional.normalize(im_feats, p=2, dim=1)
         if self.cuda:
             im_feats = im_feats.cuda()
         x_fw = torch.zeros(im_feats.size(0) + 1, im_feats.size(1))
@@ -76,8 +90,9 @@ class Evaluation(object):
 
         x_fw[:im_feats.size(0)] = im_feats
         x_bw[1 : im_feats.size(0) + 1] = im_feats
-        fw_hiddens = out[0, :im_feats.size(0), :out.size(2) // 2]
-        bw_hiddens = out[0, :im_feats.size(0), out.size(2) // 2:]
+
+        import epdb; epdb.set_trace()
+
         fw_logprob = torch.nn.functional.log_softmax(torch.autograd.Variable(
             torch.mm(fw_hiddens, x_fw.permute(1, 0)))).data
         bw_logprob = torch.nn.functional.log_softmax(torch.autograd.Variable(
@@ -99,7 +114,7 @@ class Evaluation(object):
                     # continue
                     img = Image.merge("RGB", [img.split()[0], img.split()[0], img.split()[0]])
             except AttributeError:
-                # Images with size = 1 in any dimension are useless.
+                # Images with size == 1 in any dimension are useless.
                 if np.any(np.array(img.size) == 1):
                     continue
             images.append(img)